b00b47652c5bf407c3efcd384b9899b6d276c85c
Andrey Zavadskiy <pitytuman@gmail.com>
2023-04-17 18:13:32 +0500

    added index and shop pages

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('catrgories/', views.shop_view, name='shop'),
 ]
